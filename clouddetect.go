48f27936f1a8fe0b117ece06d9865b2fc58302d1
John Barton (joho) <jrbarton@gmail.com>
2018-02-06 08:10:56 +1100

    Separate google fetching from resolving

--- clouddetect.go
+++ clouddetect.go
@@ -144,11 +144,13 @@ func resolveAmazon(ip net.IP) (string, error) {
 var domainRegexp = regexp.MustCompile(`include:([^\s]+)`)
 var ipRegexp = regexp.MustCompile(`ip\d:([^\s]+)`)
 
-func resolveGoogle(ip net.IP) (bool, error) {
+func getGoogleCIDRs() ([]*net.IPNet, error) {
 	r, err := net.LookupTXT("_cloud-netblocks.googleusercontent.com")
 	if err != nil {
-		return false, err
+		return nil, err
 	}
+
+	ranges := []*net.IPNet{}
 
 	// TXT record returns result like:
 	// v=spf1 include:_cloud-netblocks1.googleusercontent.com include:_cloud-netblocks2.googleusercontent.com include:_cloud-netblocks3.googleusercontent.com include:_cloud-netblocks4.googleusercontent.com include:_cloud-netblocks5.googleusercontent.com ?all
@@ -157,7 +159,7 @@ func resolveGoogle(ip net.IP) (bool, error) {
 		for _, subMatches := range matches {
 			r, err := net.LookupTXT(subMatches[1])
 			if err != nil {
-				return false, err
+				return nil, err
 			}
 			// v=spf1 ip4:35.190.0.0/17 ip4:35.190.128.0/18 ip4:35.190.192.0/19 ip4:35.190.224.0/20 ip4:35.192.0.0/14 ip4:35.196.0.0/15 ip4:35.198.0.0/16 ip4:35.199.0.0/17 ip4:35.199.128.0/18 ip4:35.200.0.0/15 ip4:35.204.0.0/15 ip6:2600:1900::/35 ?all
 			for _, e := range r {
@@ -166,14 +168,26 @@ func resolveGoogle(ip net.IP) (bool, error) {
 				for _, ipSubMatches := range ipMatches {
 					_, net, err := net.ParseCIDR(ipSubMatches[1])
 					if err != nil {
-						return false, err
+						return nil, err
 					}
-					if net.Contains(ip) {
-						return true, nil
-					}
+					ranges = append(ranges, net)
 				}
 			}
 
+		}
+	}
+	return ranges, nil
+}
+
+func resolveGoogle(ip net.IP) (bool, error) {
+	ranges, err := getGoogleCIDRs()
+	if err != nil {
+		return false, err
+	}
+
+	for _, cidr := range ranges {
+		if cidr.Contains(ip) {
+			return true, nil
 		}
 	}
 	return false, ErrNotCloudIP
