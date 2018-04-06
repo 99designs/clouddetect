8149cba7287b31fe565cf031b00c44e08faf1044
Nadeem Chaudhry <nadeem.chaudhry@careerbliss.com>
2018-04-06 14:37:20 -0700

    Add support for storing subnet cache to disk, including when running with multiple instances

--- clouddetect.go
+++ clouddetect.go
@@ -1,8 +1,12 @@
 package clouddetect
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
 	"sync"
 	"time"
 )
@@ -12,22 +16,29 @@ type Client struct {
 	// unexported cache storage
 	subnetCache    []*Response
 	cacheWriteTime time.Time
-	cacheMutex     *sync.Mutex
+	cacheMutex     *sync.RWMutex
+	cacheSource    string
 
 	// Time to keep IP ranges cached for (default 12 hours)
-	TTL time.Duration
+	TTL           time.Duration
+	CacheFilePath string
 }
 
 // Response provides details of the cloud environment the IP resolved to
+type diskCache struct {
+	SubnetCache []*Response `json:"cache"`
+}
 type Response struct {
-	ProviderName string
-	Region       string
-	Subnet       *net.IPNet
+	ProviderName string     `json:"providerName"`
+	Region       string     `json:"region"`
+	Subnet       *net.IPNet `json:"subnet"`
 }
 
 var (
 	// ErrNotCloudIP is error returned when IP does not match any of the published list of ranges
-	ErrNotCloudIP = errors.New("not resolved to any known cloud IP range")
+	ErrNotCloudIP      = errors.New("not resolved to any known cloud IP range")
+	ErrCacheNotReady   = errors.New("subnet cache is still being downloaded")
+	ErrCacheFileLocked = errors.New("cache lock file exists, skipping cache refresh")
 )
 
 const (
@@ -37,13 +48,15 @@ const (
 	ProviderGoogle = "Google Cloud"
 	// ProviderMicrosoft is Microsoft Azure
 	ProviderMicrosoft = "Microsoft Azure"
+	cacheSourceDisk   = "Disk"
+	cacheSourceWeb    = "Web"
 )
 
 // NewClient generates a Client with specified cache TTL
 func NewClient(TTL time.Duration) *Client {
 	return &Client{
 		TTL:        TTL,
-		cacheMutex: &sync.Mutex{},
+		cacheMutex: &sync.RWMutex{},
 	}
 }
 
@@ -62,49 +75,157 @@ func Resolve(ip net.IP) (*Response, error) {
 	return DefaultClient().Resolve(ip)
 }
 
-func (c *Client) allSubnetsForProviders() ([]*Response, error) {
-	c.cacheMutex.Lock()
-	if c.subnetCache == nil || c.cacheWriteTime.Add(c.TTL).Before(time.Now()) {
-		c.subnetCache = []*Response{}
-
-		amazon, err := getAmazonCIDRs()
-		if err != nil {
-			return nil, err
-		}
-		c.subnetCache = append(c.subnetCache, amazon...)
-
-		google, err := getGoogleCIDRs()
-		if err != nil {
-			return nil, err
-		}
-		c.subnetCache = append(c.subnetCache, google...)
-
-		microsoft, err := getMicrosoftCIDRs()
-		if err != nil {
-			return nil, err
-		}
-		c.subnetCache = append(c.subnetCache, microsoft...)
-		c.cacheWriteTime = time.Now()
-	}
-	c.cacheMutex.Unlock()
-
-	return c.subnetCache, nil
-}
-
 // Resolve will take the given ip and determine if it exists within any of the major
 // cloud providers' published IP ranges and any extra metadata that may be of use.
 // It returns ErrNotCloudIP if the IP does not resolve against any lists
-func (c *Client) Resolve(ip net.IP) (*Response, error) {
-	subNets, err := c.allSubnetsForProviders()
-	if err != nil {
-		return nil, err
+func (c *Client) Resolve(ip net.IP) (response *Response, err error) {
+	c.cacheMutex.RLock()
+	if len(c.subnetCache) == 0 || c.cacheWriteTime.Add(c.TTL).Before(time.Now()) {
+		c.cacheMutex.RUnlock()
+
+		// Ensure future checks don't trigger subsequent refreshes
+		c.cacheMutex.Lock()
+		c.cacheWriteTime = time.Now()
+		c.cacheMutex.Unlock()
+
+		// Asynchronously refresh the cache because we already have subnets in memory
+		go c.RefreshCache()
 	}
 
-	for _, subNet := range subNets {
+	c.cacheMutex.RLock()
+	// Ensure cache refreshes above were successful
+	if len(c.subnetCache) == 0 {
+		c.cacheMutex.RUnlock()
+		return nil, ErrCacheNotReady
+	}
+
+	for _, subNet := range c.subnetCache {
 		if subNet.Subnet.Contains(ip) {
+			c.cacheMutex.RUnlock()
 			return subNet, nil
 		}
 	}
 
+	c.cacheMutex.RUnlock()
 	return nil, ErrNotCloudIP
+}
+
+// Refreshes the cloud provider subnet cache first form disk (if available) and then from the web
+func (c *Client) RefreshCache() (err error) {
+	if c.CacheFilePath != "" {
+		// Always check the local cache first, it may have been updated by another process
+		if err = c.refreshCacheFromDisk(); err == nil {
+			if c.cacheWriteTime.Add(c.TTL).Before(time.Now()) {
+				// The local cache is still up to date
+				return nil
+			}
+		}
+
+		if stat, err := os.Stat(c.lockFilePath()); err == nil {
+			// Another process is refreshing the cache, ensure it's not an old lock file
+			if stat.ModTime().Add(c.TTL).Before(time.Now()) {
+				// The lock file has existed longer than expected
+				os.Remove(c.lockFilePath())
+				// Restart and generate a new lock file
+				return c.RefreshCache()
+			}
+
+			start := time.Now()
+			for start.Add(c.TTL).After(time.Now()) {
+				time.Sleep(5 * time.Second)
+				if _, err := os.Stat(c.lockFilePath()); os.IsNotExist(err) {
+					// Lock file has been removed, refresh the cache from disk
+					return c.refreshCacheFromDisk()
+				}
+			}
+
+			// The other process didn't successfully refresh the cache, await the next interval of refresh cache
+			return ErrCacheFileLocked
+		}
+
+		// This process is the one responsible for the lock file refresh.
+		if lockFile, err := os.OpenFile(c.lockFilePath(), os.O_RDONLY|os.O_CREATE, os.ModePerm); err == nil {
+			// We don't need to interact with the file, so we can close it immediately.
+			lockFile.Close()
+			defer os.Remove(lockFile.Name())
+		}
+	}
+
+	// Refresh the cache from the web
+	subnetCache := []*Response{}
+
+	amazon, err := getAmazonCIDRs()
+	if err != nil {
+		return err
+	}
+	subnetCache = append(subnetCache, amazon...)
+
+	google, err := getGoogleCIDRs()
+	if err != nil {
+		return err
+	}
+	subnetCache = append(subnetCache, google...)
+
+	microsoft, err := getMicrosoftCIDRs()
+	if err != nil {
+		return err
+	}
+	subnetCache = append(subnetCache, microsoft...)
+
+	if c.CacheFilePath != "" {
+		cache := diskCache{
+			SubnetCache: subnetCache,
+		}
+		// The > 2 check is to ensure we're not serializing an empty JSON file, i.e. {}
+		if data, err := json.MarshalIndent(cache, "", "  "); err == nil && len(data) > 2 {
+			ioutil.WriteFile(c.CacheFilePath, data, os.ModePerm)
+		}
+	}
+
+	c.cacheMutex.Lock()
+	c.subnetCache = subnetCache
+	c.cacheWriteTime = time.Now()
+	c.cacheSource = cacheSourceWeb
+	c.cacheMutex.Unlock()
+
+	return nil
+}
+
+func (c *Client) lockFilePath() (lfp string) {
+	return fmt.Sprintf("%s.lock", c.CacheFilePath)
+}
+
+// Refreshes the cloud provider subnet cache from the local cache file
+func (c *Client) refreshCacheFromDisk() (err error) {
+	f, err := os.OpenFile(c.CacheFilePath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	var cache diskCache
+	err = json.Unmarshal(data, &cache)
+	if err != nil {
+		return err
+	}
+
+	c.cacheMutex.Lock()
+	c.subnetCache = cache.SubnetCache
+	if stat, err := f.Stat(); err == nil {
+		c.cacheWriteTime = stat.ModTime()
+	}
+	c.cacheSource = cacheSourceDisk
+	c.cacheMutex.Unlock()
+
+	return nil
+}
+
+// The number of cloud provider subnets loaded in the cache
+func (c *Client) Count() (subnetCount int) {
+	return len(c.subnetCache)
 }
--- clouddetect_test.go
+++ clouddetect_test.go
@@ -1,14 +1,16 @@
 package clouddetect
 
 import (
+	"io/ioutil"
 	"net"
+	"os"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestDetect(t *testing.T) {
-	client := DefaultClient()
-
-	testCases := []struct {
+var (
+	testCases = []struct {
 		providerName string
 		ip           string
 	}{
@@ -16,6 +18,10 @@ func TestDetect(t *testing.T) {
 		{ProviderGoogle, "146.148.34.2"},
 		{ProviderMicrosoft, "168.61.66.2"},
 	}
+)
+
+func TestDetect(t *testing.T) {
+	client := DefaultClient()
 
 	for _, tc := range testCases {
 		t.Run(tc.providerName, func(t *testing.T) {
@@ -34,4 +40,152 @@ func TestDetect(t *testing.T) {
 			t.Errorf("%v resolved incorrectly with %v", badIP, err)
 		}
 	})
+}
+
+func TestThatRefreshCacheToDiskWorks(t *testing.T) {
+	tempFile, err := ioutil.TempFile(os.TempDir(), "clouddetect_test")
+	if err != nil {
+		t.Errorf("Could not create temp file for cache output: %v", err)
+		return
+	}
+	// last-in, first-out
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	client := DefaultClient()
+	client.CacheFilePath = tempFile.Name()
+
+	if err := client.RefreshCache(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if len(client.subnetCache) == 0 {
+		t.Error("client.subnetCache is empty, expected records.")
+		return
+	}
+	if stat, err := os.Stat(client.CacheFilePath); err != nil {
+		t.Errorf("Could not get cache file (%s) stat: %v", client.CacheFilePath, err)
+		return
+	} else if size := stat.Size(); size < 3 {
+		t.Errorf("Cache file is empty, but subnetCache contains %d records", len(client.subnetCache))
+		return
+	} else {
+		t.Logf("Found %d subnet records and saved to disk in %d bytes.", len(client.subnetCache), size)
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.providerName, func(t *testing.T) {
+			ip := net.ParseIP(tc.ip)
+			match, err := client.Resolve(ip)
+			if err != nil || match.ProviderName != tc.providerName {
+				t.Errorf("Expected %v to resolve to %v, got %#v:%#v", ip, tc.providerName, match, err)
+			}
+		})
+	}
+
+	t.Logf("Successfully resolved %d test cases", len(testCases))
+}
+
+func TestThatMultiProcessRefreshCacheFromDiskWorks(t *testing.T) {
+	tempFile, err := ioutil.TempFile(os.TempDir(), "clouddetect_test")
+	if err != nil {
+		t.Errorf("Could not create temp file for cache output: %v", err)
+		return
+	}
+	// last-in, first-out
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	client1 := NewClient(12 * time.Hour)
+	client1.CacheFilePath = tempFile.Name()
+
+	client2 := NewClient(12 * time.Hour)
+	client2.CacheFilePath = client1.CacheFilePath
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		if err := client1.RefreshCache(); err != nil {
+			t.Errorf("Client1: RefreshCache failed: %v", err)
+		}
+		wg.Done()
+	}()
+
+	// Ensure the lock file exists
+	time.Sleep(1 * time.Second)
+	if _, err := os.Stat(client1.lockFilePath()); os.IsNotExist(err) {
+		t.Errorf("Expected lock file to exist after starting initial RefreshCache gorouting, but file stat returned: %v", err)
+	}
+
+	wg.Add(1)
+	go func() {
+		if err := client2.RefreshCache(); err != nil {
+			t.Errorf("Client2: RefreshCache failed: %v", err)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	t.Logf("Finished refreshing cache via 2 goroutines. Found %d subnet records.", len(client1.subnetCache))
+
+	if _, err := os.Stat(client1.lockFilePath()); !os.IsNotExist(err) {
+		t.Errorf("Expected lock file IsNotExist err value after refresh cache call, but file stat returned: %v", err)
+	}
+
+	// Ensure the first process reloaded from the web
+	if client1.cacheSource != cacheSourceWeb {
+		t.Errorf("Expected client1 cache source to be %s, but received %s", cacheSourceWeb, client1.cacheSource)
+	}
+	if client2.cacheSource != cacheSourceDisk {
+		t.Errorf("Expected client2 cache source to be %s, but received %s", cacheSourceDisk, client2.cacheSource)
+	}
+	if len(client1.subnetCache) != len(client2.subnetCache) {
+		t.Errorf("Expected client1 subnetCache count (%d) to match client2 subnetCache count (%d), but it didn't", len(client1.subnetCache), len(client2.subnetCache))
+	}
+}
+
+func TestThatDeleteOldLockFileWorks(t *testing.T) {
+	tempFile, err := ioutil.TempFile(os.TempDir(), "clouddetect_test")
+	if err != nil {
+		t.Errorf("Could not create temp file for cache output: %v", err)
+		return
+	}
+	// last-in, first-out
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	client := DefaultClient()
+	client.CacheFilePath = tempFile.Name()
+
+	f, err := os.OpenFile(client.lockFilePath(), os.O_RDONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	f.Close()
+
+	if err = os.Chtimes(client.lockFilePath(), time.Now().Add(-24*time.Hour), time.Now().Add(-24*time.Hour)); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if err := client.RefreshCache(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if len(client.subnetCache) == 0 {
+		t.Error("client.subnetCache is empty, expected records.")
+		return
+	}
+	if stat, err := os.Stat(client.CacheFilePath); err != nil {
+		t.Errorf("Could not get cache file (%s) stat: %v", client.CacheFilePath, err)
+		return
+	} else if size := stat.Size(); size < 3 {
+		t.Errorf("Cache file is empty, but subnetCache contains %d records", len(client.subnetCache))
+		return
+	} else {
+		t.Logf("Found %d subnet records and saved to disk in %d bytes.", len(client.subnetCache), size)
+	}
 }
